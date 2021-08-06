143b60765bc969db1879e5d82d04d195a017612d
Danail <tdanail12@gmail.com>
2021-08-06 22:53:44 +0300

    Section 4

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const bootcamps = require('./routes/bootcamps')
 const morgan = require('morgan')
+const colors = require('colors')
+const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./config/db')
+
 dotenv.config({ path: './config/config.env' })
+connectDB()
 
 const app = express()
+
+app.use(express.json())
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+)
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  server.close(() => process.exit(1))
+})
