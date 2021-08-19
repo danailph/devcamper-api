055e02e66c23dd769a0d6ba85d3169d3a57b39b9
Danail <tdanail12@gmail.com>
2021-08-19 21:34:25 +0300

    Section 10

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,6 +27,14 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res)
 })
 
+// @desc: Logout user / clear cookie
+// @route: GET api/v1/auth/logout
+// @access: Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', { expires: new Date(Date.now() + 60 * 1000), httpOnly: true })
+  res.status(200).json({ success: true })
+})
+
 // @desc: Get current logged in user
 // @route: GET api/v1/auth/me
 // @access: Private
